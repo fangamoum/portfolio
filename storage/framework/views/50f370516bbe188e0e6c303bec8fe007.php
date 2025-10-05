bb705c16d59f90778298452fdc883b9d8fb464f6
fangamoum <157338291+fangamoum@users.noreply.github.com>
2025-10-05 20:37:12 +0200

    Add files via upload

---
+++ storage/framework/views/50f370516bbe188e0e6c303bec8fe007.php
@@ -0,0 +1,3 @@
+;
+
+<?php echo $__env->make('layouts.app', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\Users\LM FANGAMOU\Desktop\laravel\portfolio\resources\views/welcome.blade.php ENDPATH**/ ?>
